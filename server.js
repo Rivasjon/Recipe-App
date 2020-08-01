77d944270ab0f1013009f51cfc868ddde4e87c42
Rivasjon <Rivasjon000@Gmail.com>
2020-07-31 22:37:18 -0500

    adding login page

--- server.js
+++ server.js
@@ -4,18 +4,46 @@ if (process.env.NODE_ENV !== "production") {
 
 const express = require("express");
 const app = express();
+
 const expressLayouts = require("express-ejs-layouts");
-const bodyParser = require('body-parser')
+const bodyParser = require("body-parser");
 
 const indexRouter = require("./routes/index");
 const authorRouter = require("./routes/authors");
 
+// loging attempt 
+app.get('/',(req, res) => {
+  res.render('index.ejs')
+})
+ app.get('/login', (req,res) =>{
+  res.render('login.ejs')
+ })
+ app.post('/login',(req,res)=>{
+   
+})
+ app.get('/register', (req,res) =>{
+  res.render('register.ejs')
+ })
+ app.post('/register',(req,res)=>{
+req.body.name
+ })
+
+
+
+
+
+
+
+ //end attempt
+
+//keep please dont touch
 app.set("view engine", "ejs");
 app.set("views", __dirname + "/views");
 app.set("layout", "layouts/layout");
 app.use(expressLayouts);
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({limit: '10mb', extended:false  }))
+app.use(bodyParser.urlencoded({ limit: "10mb", extended: false }));
+//keep end
 
 const mongoose = require("mongoose");
 mongoose.connect(process.env.DATABASE_URL, {
@@ -27,8 +55,8 @@ const db = mongoose.connection;
 db.on("error", error => console.error(error));
 db.once("open", () => console.log("Connected to Mongoose"));
 
-app.use('/', indexRouter);
-app.use('/authors', authorRouter)
-'authors/new'
+app.use("/", indexRouter);
+app.use("/authors", authorRouter);
+("authors/new");
 
 app.listen(process.env.PORT || 3000);
